0fa6caaa2931ef83cfbba53223d979a89a62dcf9
Pranav Sethi <96.pranav@gmail.com>
2018-02-24 17:33:23 -0500

    new play

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@ header('Content-Type: application/json');
 
 if (isset($_COOKIE['ttt-session'])){
 	setcookie("ttt-session", "", (time() - 86400) , "/");
+	setcookie("ttt-game", "", (time() - 86400) , "/");
 	success();
 } else error();
 

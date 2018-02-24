bb6ae916ddafc46c5e1f5d8fd30010e22152b0d6
Pranav Sethi <96.pranav@gmail.com>
2018-02-24 05:10:39 -0500

    Login

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+require_once('config.php');
+
+header('Content-Type: application/json');
+
+$data = file_get_contents("php://input");
+$data = json_decode($data, true);
+
+if (!empty($data) && isset($data['username']) && isset($data['password'])){
+
+	$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ? AND email_verified = '1' LIMIT 1");
+	$stmt->bind_param("ss", $username, $password);
+
+	$username = $data['username'];
+	$password = $data['password'];
+
+	$stmt -> execute();
+
+	if ($stmt->rowCount() == 0){
+		error();
+	}
+
+	$res = $stmt->get_result();
+
+	while ($row = $res -> fetch_assoc()){
+		setcookie("ttt-session", json_encode($row), time() + 86400);
+	}
+
+	success();
+
+} else error();
+
+
+
+?>

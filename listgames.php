478d204b86ede894e1e4a2fd1f0c82636bfed4ef
Pranav Sethi <96.pranav@gmail.com>
2018-02-24 06:17:05 -0500

    Listgames

---
+++ listgames.php
@@ -0,0 +1,32 @@
+<?php
+require_once('config.php');
+
+header('Content-Type: application/json');
+
+
+if (isset($_COOKIE['ttt-session'])){
+
+	$user = json_decode($_COOKIE['ttt-session'],true);
+
+	$stmt = $conn->prepare("SELECT id, start_date FROM games WHERE user_id = ?");
+	$stmt->bind_param("s", $user_id);
+
+	$user_id = $user['id'];
+
+	$stmt -> execute();
+
+	$res = $stmt->get_result();
+
+	$response = array('status' => "OK", 'games' => array());
+
+	while ($row = $res -> fetch_assoc()){
+		array_push($response['games'], $row);
+	}
+
+	echo json_encode($response, JSON_PRETTY_PRINT);
+
+} else error();
+
+
+
+?>

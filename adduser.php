f3846683ec2d378f9389e15de4d4c60fde58d570
Pranav Sethi <96.pranav@gmail.com>
2018-02-25 14:52:39 -0500

    new ui

--- adduser.php
+++ adduser.php
@@ -9,7 +9,7 @@ $data = json_decode($data, true);
 if (!empty($data) && isset($data['username']) && isset($data['password']) && isset($data['email']) ){
 
 	$stmt = $conn->prepare("INSERT INTO users (username, password, email, key__) VALUES (?, ?, ?, ?)");
-	$stmt->bind_param("sss", $username, $password, $email, $key);
+	$stmt->bind_param("ssss", $username, $password, $email, $key);
 
 	$username = $data['username'];
 	$password = $data['password'];
